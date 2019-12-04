26e8726f29b4ce1e7f2a976009e49554de6a9632
nasaku898 <simonlim898@hotmail.com>
2019-12-03 20:29:36 -0500

    Added Registration Service and implementation

--- restaurantList/src/main/java/com/Simon/restaurantList/service/RegistrationService.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/service/RegistrationService.java
@@ -1,6 +1,10 @@
 package com.Simon.restaurantList.service;
 
+import com.Simon.restaurantList.exceptions.InvalidEmailException;
+import com.Simon.restaurantList.model.UserModel;
+
 public interface RegistrationService {
 	
-	public void createNewUser();
+	public boolean createNewUser(UserModel newUser);
+	public boolean validateEmail(String email) throws InvalidEmailException;
 }
--- restaurantList/src/main/java/com/Simon/restaurantList/service/serviceImpl/DefaultRegistrationService.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/service/serviceImpl/DefaultRegistrationService.java
@@ -1,22 +1,42 @@
 package com.Simon.restaurantList.service.serviceImpl;
 
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.Simon.restaurantList.dao.UserDao;
+import com.Simon.restaurantList.exceptions.InvalidEmailException;
+import com.Simon.restaurantList.model.UserModel;
 import com.Simon.restaurantList.service.RegistrationService;
 
+//Added password validation in future and email
 public class DefaultRegistrationService implements RegistrationService {
-	
+
 	UserDao userDao;
-	
-	public DefaultRegistrationService(@Qualifier("UserDao") UserDao userDao)
-	{
-		
+
+	@Autowired
+	public DefaultRegistrationService(@Qualifier("UserDao") UserDao userDao) {
+		this.userDao = userDao;
 	}
+
 	@Override
-	public void createNewUser() {
-		// TODO Auto-generated method stub
-		
+	public boolean createNewUser(UserModel newUser) {
+		try {
+			validateEmail(newUser.getEmailAdress());
+			userDao.save(newUser);
+			return true;
+		} catch (InvalidEmailException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+
 	}
-	
+
+	//Add validation for repeated email already in DB
+	@Override
+	public boolean validateEmail(String email) throws InvalidEmailException {
+		String regex = "\\w+\\@\\w+\\.[A-Za-z]+";
+		return Pattern.matches(regex, email);
+	}
+
 }
