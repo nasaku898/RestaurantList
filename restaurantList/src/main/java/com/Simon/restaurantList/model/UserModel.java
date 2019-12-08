750415b7ec48e26b55f1ca07861edb3838ec8327
nasaku898 <simonlim898@hotmail.com>
2019-12-07 23:20:34 -0500

    Added role and permission to UserModel

--- restaurantList/src/main/java/com/Simon/restaurantList/model/UserModel.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/model/UserModel.java
@@ -1,5 +1,8 @@
 package com.Simon.restaurantList.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -12,11 +15,12 @@ public class UserModel {
 	private String username;
 	private String password;
 	private String emailAdress;
-
+	private String roles = "";
+	private String permissions = "";
+	
 	public UserModel(@JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName,
-			@JsonProperty("username") String username, @JsonProperty("passwrod") String password,
-			@JsonProperty("emailAdress") String emailAdress) 
-	{
+			@JsonProperty("username") String username, @JsonProperty("password") String password,
+			@JsonProperty("emailAdress") String emailAdress) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.username = username;
@@ -71,7 +75,27 @@ public class UserModel {
 	public void setEmailAdress(String emailAdress) {
 		this.emailAdress = emailAdress;
 	}
-	
-	
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+
+	public void setPermissions(String permissions) {
+		this.permissions = permissions;
+	}
+
+	public List<String> getRoleList() {
+		if (this.roles.length() > 0) {
+			return Arrays.asList(this.roles.split(","));
+		}
+		return new ArrayList<>();
+	}
+
+	public List<String> getPermissionList() {
+		if (this.permissions.length() > 0) {
+			return Arrays.asList(this.permissions.split(","));
+		}
+		return new ArrayList<>();
+	}
 
 }
