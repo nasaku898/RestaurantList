a9aae4609070251ee1cf099c88eda483d5c77c3c
nasaku898 <simonlim898@hotmail.com>
2019-12-07 23:13:07 -0500

    Added Spring Security Config File

--- restaurantList/src/main/java/com/Simon/restaurantList/security/UserDetailsServiceImpl.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/security/UserDetailsServiceImpl.java
@@ -7,15 +7,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.Simon.restaurantList.dao.UserDao;
 import com.Simon.restaurantList.model.UserModel;
 
+@Service("defaultUserDetailServiceImpl")
 public class UserDetailsServiceImpl implements UserDetailsService {
 	UserDao userDao;
 	
 	@Autowired
-	public UserDetailsServiceImpl(@Qualifier("UserDao") UserDao userDao) {
+	public UserDetailsServiceImpl(@Qualifier("userDaoImpl") UserDao userDao) {
 		this.userDao = userDao;
 	}
 	
--- restaurantList/src/main/java/com/Simon/restaurantList/security/config/SecurityConfiguration.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/security/config/SecurityConfiguration.java
@@ -1,15 +1,54 @@
 package com.Simon.restaurantList.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.Simon.restaurantList.security.UserDetailsServiceImpl;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-	public void configure(HttpSecurity httpSecurity) {
-		
+
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	public SecurityConfiguration(@Qualifier("defaultUserDetailServiceImpl") UserDetailsService userDetailsService) {
+		super();
+		this.userDetailsService = userDetailsService;
+	}
+
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	// implement login form later
+	public void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("/register").permitAll().antMatchers("/authentication/**")
+				.authenticated().antMatchers("/User/users").hasRole("ADMIN").antMatchers("/Users/**").authenticated()
+				.and().httpBasic().and().csrf().disable();
+		;
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+		return daoAuthenticationProvider;
 	}
 }
