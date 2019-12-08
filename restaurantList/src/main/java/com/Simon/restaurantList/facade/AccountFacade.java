2d187af6a04e2253b5df35728bbcddcb43085214
nasaku898 <simonlim898@hotmail.com>
2019-12-07 23:19:34 -0500

    Added new methods to AccountFacade

--- restaurantList/src/main/java/com/Simon/restaurantList/facade/AccountFacade.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/facade/AccountFacade.java
@@ -1,14 +1,22 @@
 package com.Simon.restaurantList.facade;
 
+import java.util.List;
+
 import com.Simon.restaurantList.model.UserModel;
 
 public interface AccountFacade {
-	public int updateEmail(String newEmail, UserModel currentUser);
+	public int updateEmail(String newEmail);
 
-	public int updateName(String newUserName, String newLastName, UserModel currentUser);
+	public int updateName(String newUserName, String newLastName);
 
-	public int updatePassword(String newPassword, UserModel currentUser);
+	public int updatePassword(String newPassword);
 
 	public boolean createNewUser(UserModel newUser);
+
+	public UserModel getUserByEmail(String email);
+
+	public List<UserModel> getAllUsers();
+
+	public UserModel getCurrentUser();
 
 }
--- restaurantList/src/main/java/com/Simon/restaurantList/facade/Impl/DefaultAccountFacade.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/facade/Impl/DefaultAccountFacade.java
@@ -1,7 +1,10 @@
 package com.Simon.restaurantList.facade.Impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 import com.Simon.restaurantList.exceptions.InvalidEmailException;
 import com.Simon.restaurantList.facade.AccountFacade;
@@ -9,20 +12,23 @@ import com.Simon.restaurantList.model.UserModel;
 import com.Simon.restaurantList.service.RegistrationService;
 import com.Simon.restaurantList.service.UserService;
 
+@Component("defaultAccountFacade")
 public class DefaultAccountFacade implements AccountFacade {
 
 	UserService userService;
 	RegistrationService registrationService;
 
 	@Autowired
-	public DefaultAccountFacade(@Qualifier("UserService") UserService userService,
-			@Qualifier("registrationService") RegistrationService registrationService) {
+	public DefaultAccountFacade(@Qualifier("defaultUserService") UserService userService,
+			@Qualifier("defaultRegistrationService") RegistrationService registrationService) {
 		this.userService = userService;
 		this.registrationService = registrationService;
 	}
 
 	@Override
-	public int updateEmail(String newEmail, UserModel currentUser) {
+	public int updateEmail(String newEmail) {
+		UserModel currentUser = getCurrentUser();
+
 		currentUser.setEmailAdress(newEmail);
 		try {
 			return userService.updateEmail(currentUser);
@@ -32,14 +38,22 @@ public class DefaultAccountFacade implements AccountFacade {
 	}
 
 	@Override
-	public int updateName(String newFirstName, String newLastName, UserModel currentUser) {
+	public int updateName(String newFirstName, String newLastName) {
+		UserModel currentUser = getCurrentUser();
+		
+		if(newFirstName == null || newLastName == null)
+		{
+			throw new IllegalStateException("firstName or lastName field is empty");
+		}
 		currentUser.setFirstName(newFirstName);
 		currentUser.setLastName(newLastName);
 		return userService.updateName(currentUser);
 	}
-
+	
+	//add password encoder
 	@Override
-	public int updatePassword(String newPassword, UserModel currentUser) {
+	public int updatePassword(String newPassword) {
+		UserModel currentUser = getCurrentUser();
 		currentUser.setPassword(newPassword);
 		return userService.updatePassword(currentUser);
 	}
@@ -47,6 +61,21 @@ public class DefaultAccountFacade implements AccountFacade {
 	@Override
 	public boolean createNewUser(UserModel newUser) {
 		return registrationService.createNewUser(newUser);
+	}
+
+	@Override
+	public UserModel getUserByEmail(String email) {
+		return userService.getUserByEmail(email);
+	}
+
+	@Override
+	public List<UserModel> getAllUsers() {
+		return userService.getAllUsers();
+	}
+
+	@Override
+	public UserModel getCurrentUser() {
+		return userService.getCurrentUser();
 	}
 
 }
