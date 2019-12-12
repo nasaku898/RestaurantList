96ffefd60bd2546a42d615518fa624c7daf964e9
nasaku898 <simonlim898@hotmail.com>
2019-12-11 23:14:25 -0500

    Removed unused import

--- restaurantList/src/main/java/com/Simon/restaurantList/controller/AuthenticationController.java
+++ restaurantList/src/main/java/com/Simon/restaurantList/controller/AuthenticationController.java
@@ -1,14 +1,8 @@
 package com.Simon.restaurantList.controller;
 
-import java.util.Map;
-
-import javax.net.ssl.SSLEngineResult.Status;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +31,7 @@ public class AuthenticationController {
 	{
 		user.setRoles("USER");
 		accountFacade.createNewUser(user);
-	}
+	} 
 	
 	
 	@RequestMapping(value = "/authentication/login" , method=RequestMethod.GET)
